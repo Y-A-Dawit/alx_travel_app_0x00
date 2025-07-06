f6eebf5c2947d4b172520e4428cebb16eafab912
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-07-06 18:31:16 +0300

    update

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -1,8 +1,11 @@
 import environ
 import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.ENV'))
 
 
 """
